1175f0b7ce31f3f0ebb111dd6ae715ff28d6373f
algebraic-arima <lifangke@sjtu.edu.cn>
2024-09-03 23:23:03 +0800

    push failed

--- src/Optim/Mem2Reg/Mem2Reg.java
+++ src/Optim/Mem2Reg/Mem2Reg.java
@@ -1,4 +1,13 @@
 package src.Optim.Mem2Reg;
 
+import src.IR.IRDef.IRFuncDef;
+import src.IR.IRProg;
+
 public class Mem2Reg {
+
+    public Mem2Reg(IRProg node) {
+        for (IRFuncDef func : node.funcDefs) {
+
+        }
+    }
 }
